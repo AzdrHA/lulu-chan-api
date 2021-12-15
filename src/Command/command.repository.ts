e071e242fd48bd40f2acd86f53147be904333cf1
Azdra <baptiste.brand2002@gmail.com>
2021-12-15 21:58:57 +0100

    Create getListByCategory query builder

--- src/Command/command.repository.ts
+++ src/Command/command.repository.ts
@@ -1,5 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { CommandEntity } from './command.entity';
+import { CommandCategoryEntity } from '../CommandCategory/command.category.entity';
 
 @EntityRepository(CommandEntity)
-export class CommandRepository extends Repository<CommandEntity> {}
+export class CommandRepository extends Repository<CommandEntity> {
+  public getListByCategory(category: CommandCategoryEntity) {
+    return this.createQueryBuilder('c')
+      .where('c.category = :category')
+      .setParameter('category', category.id)
+      .getMany();
+  }
+}
