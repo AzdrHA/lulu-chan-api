e9f73ef3b26fc704ae685c8d9ddd52e1f93ef236
Azdra <baptiste.brand2002@gmail.com>
2021-12-15 21:59:54 +0100

    Improve uploadFile fn and add this code in his service

--- src/Image/image.controller.ts
+++ src/Image/image.controller.ts
@@ -3,41 +3,29 @@ import {
   Controller,
   Post,
   Req,
-  Res,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Request } from 'express';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { imageInterceptor } from 'src/Interceptor/image.interceptor';
-import { ImageFormatRepository } from '../ImageFormat/image.format.repository';
-import { CommandRepository } from '../Command/command.repository';
+import { Image } from '../Types/Image';
+import { ImageService } from './image.service';
 
 @Controller('image')
 export class ImageController {
-  private imageFormatRepository: ImageFormatRepository;
-  private commandRepository: CommandRepository;
-
-  constructor(
-    imageFormatRepository: ImageFormatRepository,
-    commandRepository: CommandRepository,
-  ) {
-    this.imageFormatRepository = imageFormatRepository;
-    this.commandRepository = commandRepository;
+  private imageService: ImageService;
+  constructor(imageService: ImageService) {
+    this.imageService = imageService;
   }
 
   @Post('/')
   @UseInterceptors(FilesInterceptor('images', null, imageInterceptor))
   public async uploadFile(
-    @Res() res: Response,
-    @Req() req: Request,
-    @UploadedFiles() files: any,
+    @Req() request: Request,
+    @UploadedFiles() files: Image[],
     @Body() data: any,
   ) {
-    // const image = this.imageFormatRepository.create(f);
-    console.log(files);
-    return res.status(200).json({
-      t: 't',
-    });
+    return this.imageService.uploadFile(request, files);
   }
 }
