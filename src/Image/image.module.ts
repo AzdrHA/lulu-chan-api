39d8c26848301f030ddf1f1f48638784ebe4f007
Azdra <baptiste.brand2002@gmail.com>
2021-12-15 22:00:44 +0100

    Add ImageRepository

--- src/Image/image.module.ts
+++ src/Image/image.module.ts
@@ -4,10 +4,15 @@ import { ImageService } from './image.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ImageFormatRepository } from '../ImageFormat/image.format.repository';
 import { CommandRepository } from '../Command/command.repository';
+import { ImageRepository } from './image.repository';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ImageFormatRepository, CommandRepository]),
+    TypeOrmModule.forFeature([
+      ImageRepository,
+      ImageFormatRepository,
+      CommandRepository,
+    ]),
   ],
   controllers: [ImageController],
   providers: [ImageService],
