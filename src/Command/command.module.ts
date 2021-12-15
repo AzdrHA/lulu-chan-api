e0afcca4219c0923a3698b15d9f5299a4817ccc2
Azdra <baptiste.brand2002@gmail.com>
2021-12-15 21:58:45 +0100

    Add CommandCategoryRepository

--- src/Command/command.module.ts
+++ src/Command/command.module.ts
@@ -3,9 +3,12 @@ import { CommandController } from './command.controller';
 import { CommandService } from './command.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommandRepository } from './command.repository';
+import { CommandCategoryRepository } from '../CommandCategory/command.category.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommandRepository])],
+  imports: [
+    TypeOrmModule.forFeature([CommandRepository, CommandCategoryRepository]),
+  ],
   controllers: [CommandController],
   providers: [CommandService],
 })
