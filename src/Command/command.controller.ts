5452a8edf4580de4258c8b02149d281c1ae66a3a
Azdra <baptiste.brand2002@gmail.com>
2021-12-15 21:58:23 +0100

    Add fn createCommand and put this code in his service

--- src/Command/command.controller.ts
+++ src/Command/command.controller.ts
@@ -1,1 +1,21 @@
-export class CommandController {}
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { Response, Request } from 'express';
+import { CommandEntity } from './command.entity';
+import { CommandService } from './command.service';
+
+@Controller('command')
+export class CommandController {
+  private commandService: CommandService;
+  constructor(commandService: CommandService) {
+    this.commandService = commandService;
+  }
+
+  @Post('/')
+  public async createCommand(
+    @Res() res: Response,
+    @Body() data: CommandEntity,
+    @Req() request: Request,
+  ) {
+    return this.commandService.createCommand(request, data);
+  }
+}
