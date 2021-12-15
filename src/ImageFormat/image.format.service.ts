6f9c535d7c2204031a8a671d771202c0d931c22c
Azdra <baptiste.brand2002@gmail.com>
2021-12-15 22:00:15 +0100

    Create ImageFormatService

--- src/ImageFormat/image.format.service.ts
+++ src/ImageFormat/image.format.service.ts
@@ -1,1 +1,64 @@
-export class ImageFormatService {}
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { ImageFormatRepository } from './image.format.repository';
+import { Request } from 'express';
+import { ImageFormatEntity } from './image.format.entity';
+
+@Injectable()
+export class ImageFormatService {
+  private imageFormatRepository: ImageFormatRepository;
+
+  public constructor(imageFormatRepository: ImageFormatRepository) {
+    this.imageFormatRepository = imageFormatRepository;
+  }
+
+  /**
+   * Create image format
+   * @param {Request} request
+   * @param {ImageFormatEntity} data
+   * @return {Response}
+   */
+  public async createFormat(request: Request, data: ImageFormatEntity) {
+    const format = this.imageFormatRepository.create(data);
+
+    try {
+      await this.imageFormatRepository.save(format);
+      return request.res.status(HttpStatus.CREATED).json({
+        message: 'Mimetype successfully created',
+      });
+    } catch (e) {
+      switch (e.code) {
+        case 'ER_DUP_ENTRY':
+          return request.res.status(HttpStatus.CONFLICT).json({
+            error: 'Mimetype already existed',
+          });
+        default:
+          request.res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
+      }
+    }
+  }
+
+  /**
+   * Delete image format
+   * @param {Request} request
+   * @param {number} id
+   * @return {Response}
+   */
+  public async deleteFormat(request: Request, id: number) {
+    const format = await this.imageFormatRepository.findOne(id);
+    if (!format)
+      return request.res.status(HttpStatus.NOT_FOUND).json({
+        error: `Mimetype not found`,
+      });
+
+    try {
+      await this.imageFormatRepository.remove(format);
+      return request.res.status(HttpStatus.OK).json({
+        message: `Mimetype: "${format.mimetype}" has been removed`,
+      });
+    } catch (e) {
+      return request.res.status(HttpStatus.CONFLICT).json({
+        error: 'Impossible to remove this mimetype',
+      });
+    }
+  }
+}
